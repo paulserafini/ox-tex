de07e75a4e07f1f50fd27988b469e5e4910b6a20
paulserafini <pserafin@sfu.ca>
2019-05-15 01:57:00 -0700

    Remove preamble and polyglossia

--- ox-plaintex.el
+++ ox-plaintex.el
@@ -204,91 +204,91 @@
     ("uk" . "ukrainian"))
   "Alist between language code and corresponding Babel option.")
 
-(defconst org-latex-polyglossia-language-alist
-  '(("am" "amharic")
-    ("ast" "asturian")
-    ("ar" "arabic")
-    ("bo" "tibetan")
-    ("bn" "bengali")
-    ("bg" "bulgarian")
-    ("br" "breton")
-    ("bt-br" "brazilian")
-    ("ca" "catalan")
-    ("cop" "coptic")
-    ("cs" "czech")
-    ("cy" "welsh")
-    ("da" "danish")
-    ("de" "german" "german")
-    ("de-at" "german" "austrian")
-    ("de-de" "german" "german")
-    ("dv" "divehi")
-    ("el" "greek")
-    ("en" "english" "usmax")
-    ("en-au" "english" "australian")
-    ("en-gb" "english" "uk")
-    ("en-nz" "english" "newzealand")
-    ("en-us" "english" "usmax")
-    ("eo" "esperanto")
-    ("es" "spanish")
-    ("et" "estonian")
-    ("eu" "basque")
-    ("fa" "farsi")
-    ("fi" "finnish")
-    ("fr" "french")
-    ("fu" "friulan")
-    ("ga" "irish")
-    ("gd" "scottish")
-    ("gl" "galician")
-    ("he" "hebrew")
-    ("hi" "hindi")
-    ("hr" "croatian")
-    ("hu" "magyar")
-    ("hy" "armenian")
-    ("id" "bahasai")
-    ("ia" "interlingua")
-    ("is" "icelandic")
-    ("it" "italian")
-    ("kn" "kannada")
-    ("la" "latin" "modern")
-    ("la-modern" "latin" "modern")
-    ("la-classic" "latin" "classic")
-    ("la-medieval" "latin" "medieval")
-    ("lo" "lao")
-    ("lt" "lithuanian")
-    ("lv" "latvian")
-    ("mr" "maranthi")
-    ("ml" "malayalam")
-    ("nl" "dutch")
-    ("nb" "norsk")
-    ("nn" "nynorsk")
-    ("nko" "nko")
-    ("no" "norsk")
-    ("oc" "occitan")
-    ("pl" "polish")
-    ("pms" "piedmontese")
-    ("pt" "portuges")
-    ("rm" "romansh")
-    ("ro" "romanian")
-    ("ru" "russian")
-    ("sa" "sanskrit")
-    ("hsb" "usorbian")
-    ("dsb" "lsorbian")
-    ("sk" "slovak")
-    ("sl" "slovenian")
-    ("se" "samin")
-    ("sq" "albanian")
-    ("sr" "serbian")
-    ("sv" "swedish")
-    ("syr" "syriac")
-    ("ta" "tamil")
-    ("te" "telugu")
-    ("th" "thai")
-    ("tk" "turkmen")
-    ("tr" "turkish")
-    ("uk" "ukrainian")
-    ("ur" "urdu")
-    ("vi" "vietnamese"))
-  "Alist between language code and corresponding Polyglossia option")
+;; (defconst org-latex-polyglossia-language-alist
+;;   '(("am" "amharic")
+;;     ("ast" "asturian")
+;;     ("ar" "arabic")
+;;     ("bo" "tibetan")
+;;     ("bn" "bengali")
+;;     ("bg" "bulgarian")
+;;     ("br" "breton")
+;;     ("bt-br" "brazilian")
+;;     ("ca" "catalan")
+;;     ("cop" "coptic")
+;;     ("cs" "czech")
+;;     ("cy" "welsh")
+;;     ("da" "danish")
+;;     ("de" "german" "german")
+;;     ("de-at" "german" "austrian")
+;;     ("de-de" "german" "german")
+;;     ("dv" "divehi")
+;;     ("el" "greek")
+;;     ("en" "english" "usmax")
+;;     ("en-au" "english" "australian")
+;;     ("en-gb" "english" "uk")
+;;     ("en-nz" "english" "newzealand")
+;;     ("en-us" "english" "usmax")
+;;     ("eo" "esperanto")
+;;     ("es" "spanish")
+;;     ("et" "estonian")
+;;     ("eu" "basque")
+;;     ("fa" "farsi")
+;;     ("fi" "finnish")
+;;     ("fr" "french")
+;;     ("fu" "friulan")
+;;     ("ga" "irish")
+;;     ("gd" "scottish")
+;;     ("gl" "galician")
+;;     ("he" "hebrew")
+;;     ("hi" "hindi")
+;;     ("hr" "croatian")
+;;     ("hu" "magyar")
+;;     ("hy" "armenian")
+;;     ("id" "bahasai")
+;;     ("ia" "interlingua")
+;;     ("is" "icelandic")
+;;     ("it" "italian")
+;;     ("kn" "kannada")
+;;     ("la" "latin" "modern")
+;;     ("la-modern" "latin" "modern")
+;;     ("la-classic" "latin" "classic")
+;;     ("la-medieval" "latin" "medieval")
+;;     ("lo" "lao")
+;;     ("lt" "lithuanian")
+;;     ("lv" "latvian")
+;;     ("mr" "maranthi")
+;;     ("ml" "malayalam")
+;;     ("nl" "dutch")
+;;     ("nb" "norsk")
+;;     ("nn" "nynorsk")
+;;     ("nko" "nko")
+;;     ("no" "norsk")
+;;     ("oc" "occitan")
+;;     ("pl" "polish")
+;;     ("pms" "piedmontese")
+;;     ("pt" "portuges")
+;;     ("rm" "romansh")
+;;     ("ro" "romanian")
+;;     ("ru" "russian")
+;;     ("sa" "sanskrit")
+;;     ("hsb" "usorbian")
+;;     ("dsb" "lsorbian")
+;;     ("sk" "slovak")
+;;     ("sl" "slovenian")
+;;     ("se" "samin")
+;;     ("sq" "albanian")
+;;     ("sr" "serbian")
+;;     ("sv" "swedish")
+;;     ("syr" "syriac")
+;;     ("ta" "tamil")
+;;     ("te" "telugu")
+;;     ("th" "thai")
+;;     ("tk" "turkmen")
+;;     ("tr" "turkish")
+;;     ("uk" "ukrainian")
+;;     ("ur" "urdu")
+;;     ("vi" "vietnamese"))
+;;   "Alist between language code and corresponding Polyglossia option")
 
 
 
@@ -1158,58 +1158,58 @@ Return the new header."
 		    ", ")
 	 t nil header 1)))))
 
-(defun org-latex-guess-polyglossia-language (header info)
-  "Set the Polyglossia language according to the LANGUAGE keyword.
+;; (defun org-latex-guess-polyglossia-language (header info)
+;;   "Set the Polyglossia language according to the LANGUAGE keyword.
 
-HEADER is the LaTeX header string.  INFO is the plist used as
-a communication channel.
+;; HEADER is the LaTeX header string.  INFO is the plist used as
+;; a communication channel.
 
-Insertion of guessed language only happens when the Polyglossia
-package has been explicitly loaded.
+;; Insertion of guessed language only happens when the Polyglossia
+;; package has been explicitly loaded.
 
-The argument to Polyglossia may be \"AUTO\" which is then
-replaced with the language of the document or
-`org-export-default-language'.  Note, the language is really set
-using \setdefaultlanguage and not as an option to the package.
+;; The argument to Polyglossia may be \"AUTO\" which is then
+;; replaced with the language of the document or
+;; `org-export-default-language'.  Note, the language is really set
+;; using \setdefaultlanguage and not as an option to the package.
 
-Return the new header."
-  (let ((language (plist-get info :language)))
-    ;; If no language is set or Polyglossia is not loaded, return
-    ;; HEADER as-is.
-    (if (or (not (stringp language))
-	    (not (string-match
-		  "\\\\usepackage\\(?:\\[\\([^]]+?\\)\\]\\){polyglossia}\n"
-		  header)))
-	header
-      (let* ((options (org-string-nw-p (match-string 1 header)))
-	     (languages (and options
-			     ;; Reverse as the last loaded language is
-			     ;; the main language.
-			     (nreverse
-			      (delete-dups
-			       (save-match-data
-				 (org-split-string
-				  (replace-regexp-in-string
-				   "AUTO" language options t)
-				  ",[ \t]*"))))))
-	     (main-language-set
-	      (string-match-p "\\\\setmainlanguage{.*?}" header)))
-	(replace-match
-	 (concat "\\usepackage{polyglossia}\n"
-		 (mapconcat
-		  (lambda (l)
-		    (let ((l (or (assoc l org-latex-polyglossia-language-alist)
-				 l)))
-		      (format (if main-language-set "\\setotherlanguage%s{%s}\n"
-				(setq main-language-set t)
-				"\\setmainlanguage%s{%s}\n")
-			      (if (and (consp l) (= (length l) 3))
-				  (format "[variant=%s]" (nth 2 l))
-				"")
-			      (nth 1 l))))
-		  languages
-		  ""))
-	 t t header 0)))))
+;; Return the new header."
+;;   (let ((language (plist-get info :language)))
+;;     ;; If no language is set or Polyglossia is not loaded, return
+;;     ;; HEADER as-is.
+;;     (if (or (not (stringp language))
+;; 	    (not (string-match
+;; 		  "\\\\usepackage\\(?:\\[\\([^]]+?\\)\\]\\){polyglossia}\n"
+;; 		  header)))
+;; 	header
+;;       (let* ((options (org-string-nw-p (match-string 1 header)))
+;; 	     (languages (and options
+;; 			     ;; Reverse as the last loaded language is
+;; 			     ;; the main language.
+;; 			     (nreverse
+;; 			      (delete-dups
+;; 			       (save-match-data
+;; 				 (org-split-string
+;; 				  (replace-regexp-in-string
+;; 				   "AUTO" language options t)
+;; 				  ",[ \t]*"))))))
+;; 	     (main-language-set
+;; 	      (string-match-p "\\\\setmainlanguage{.*?}" header)))
+;; 	(replace-match
+;; 	 (concat "\\usepackage{polyglossia}\n"
+;; 		 (mapconcat
+;; 		  (lambda (l)
+;; 		    (let ((l (or (assoc l org-latex-polyglossia-language-alist)
+;; 				 l)))
+;; 		      (format (if main-language-set "\\setotherlanguage%s{%s}\n"
+;; 				(setq main-language-set t)
+;; 				"\\setmainlanguage%s{%s}\n")
+;; 			      (if (and (consp l) (= (length l) 3))
+;; 				  (format "[variant=%s]" (nth 2 l))
+;; 				"")
+;; 			      (nth 1 l))))
+;; 		  languages
+;; 		  ""))
+;; 	 t t header 0)))))
 
 (defun org-latex--remove-packages (pkg-alist info)
   "Remove packages based on the current LaTeX compiler.
@@ -1390,43 +1390,6 @@ INFO is a plist used as a communication channel."
 
 ;;; Template
 
-;;;###autoload
-(defun org-latex-make-preamble (info &optional template snippet?)
-  "Return a formatted LaTeX preamble.
-INFO is a plist used as a communication channel.  Optional
-argument TEMPLATE, when non-nil, is the header template string,
-as expected by `org-splice-latex-header'.  When SNIPPET? is
-non-nil, only includes packages relevant to image generation, as
-specified in `org-latex-default-packages-alist' or
-`org-latex-packages-alist'."
-  (let* ((class (plist-get info :latex-class))
-	 (class-template
-	  (or template
-	      (let* ((class-options (plist-get info :latex-class-options))
-		     (header (nth 1 (assoc class (plist-get info :latex-classes)))))
-		(and (stringp header)
-		     (if (not class-options) header
-		       (replace-regexp-in-string
-			"^[ \t]*\\\\documentclass\\(\\(\\[[^]]*\\]\\)?\\)"
-			class-options header t nil 1))))
-	      (user-error "Unknown LaTeX class `%s'" class))))
-    (org-latex-guess-polyglossia-language
-     (org-latex-guess-babel-language
-      (org-latex-guess-inputenc
-       (org-element-normalize-string
-	(org-splice-latex-header
-	 class-template
-	 (org-latex--remove-packages org-latex-default-packages-alist info)
-	 (org-latex--remove-packages org-latex-packages-alist info)
-	 snippet?
-	 (mapconcat #'org-element-normalize-string
-		    (list (plist-get info :latex-header)
-			  (and (not snippet?)
-			       (plist-get info :latex-header-extra)))
-		    ""))))
-      info)
-     info)))
-
 (defun org-latex-template (contents info)
   "Return complete document string after LaTeX conversion.
 CONTENTS is the transcoded contents string.  INFO is a plist
@@ -1439,8 +1402,6 @@ holding export options."
 	  (format-time-string "%% Created %Y-%m-%d %a %H:%M\n"))
      ;; LaTeX compiler.
      (org-latex--insert-compiler info)
-     ;; Document class and packages.
-     (org-latex-make-preamble info)
      ;; Possibly limit depth for headline numbering.
      (let ((sec-num (plist-get info :section-numbers)))
        (when (integerp sec-num)
